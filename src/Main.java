ff955c24e87211ebc0933812776b3969528619b3
Sharif Sharipov <sharif.sharipov@gmail.com>
2020-02-01 17:29:02 -0500

    First commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String srt = " ";
+    }
+
+    public static void sum(int a, int b){
+        System.out.print(a + b);
+    }
+
+
+}
